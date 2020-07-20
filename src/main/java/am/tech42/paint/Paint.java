be91fc56e444388813d8ba164e51bc72c3436ac0
Tigran Mkrtchyan <Tigranmkrtchyan.07.06@gmail.com>
2020-07-20 11:42:19 +0400

    Was add drawShape method. It draw  a shape  in the board using the points that it take as a parameter.

--- src/main/java/am/tech42/paint/Paint.java
+++ src/main/java/am/tech42/paint/Paint.java
@@ -4,34 +4,80 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Paint {
-	private static final char[][] board = new char[10][60];
-	private static Point curentPoint;
-	private static Queue <Point> queue = new  LinkedList <> ();
-	private static int boardWidth  = board[0].length;
-	private static int boardLength = board.length;
-	public static void main(String[] args) {
+	private final char[][] board ; 
+	private Point curentPoint;
+	private Queue <Point> queue ; 
+	private  int boardWidth ; 
+	private  int boardLength ;
+
+	
+	public Paint (int column, int row) {
+		board = new char[column][row];
+		boardWidth = board[0].length;
+		boardLength = board.length;
+		queue = new  LinkedList <> ();
 		
 		for(int i = 0; i < boardLength; i++) {
 			for(int j = 0; j < boardWidth; j++) {
 				board[i][j] = ' ';
 			}
 		}
-		for(int c = 2; c < 8; c++) {
-			board[c][5] = '*';
-			board[c][50] = '*';
-		}
-		for(int r = 5; r <= 50; r++) {
-			board[2][r] = '*';
-			board[8][r] = '*';
-		}
-		
-		fill(5,40);
-		
-		printBoard();
 	}
-	private static void fill(int i, int j) {
+	
+	public   void drawShape(Point ...points ) {
+		for(int i = 1; i<points.length; i++) {
+			drawLine(points[i-1],points[i]);
+		}
+		
+	}
+	private  void drawLine(Point p1, Point p2) {
+		int x1 = p1.getX();
+		int x2 = p2.getX();
+		int y1 = p1.getY();
+		int y2 = p2.getY();
+		if(y1 < y2) {
+			for (int c = y1; c < y2 ; c++) {
+				board[x1][c] = '*';
+			} 
+		}
+		else {
+			for (int c = y1; c > y2 ; c--) {
+				board[x1][c] = '*';
+			} 
+		}
+		if(x1 < x2) {
+			for (int r = x1; r < x2 ; r++) {
+				board[r][y2] = '*';
+			}
+		}
+		else {
+			for (int r = x1; r > x2 ; r--) {
+				board[r][y2] = '*';
+			}
+		}
+			
+	}
+	
+	public static void main(String[] args) {
+		Paint paint =new Paint(15,60);
+		Point p1 = new Point(5,2);
+		Point p2 = new Point(3,10);
+		Point p3 = new Point(5,25);
+		Point p4 = new Point(12,55);
+		
+		paint.drawShape(p1,p2,p3,p4,p1);
+		
+		paint.fill(14,6);
+		
+		paint.printBoard();
+	}
+	private  void fill(int i, int j) {
 		if(i < 0 || i > boardLength || j < 0 || j > boardWidth) {
 			throw new IllegalArgumentException();
+		}
+		while(board[i][j] =='*') {
+			i++;
+			j++;
 		}
 		queue.add(new Point (i,j));
 		board[i][j] ='*';
@@ -44,7 +90,7 @@ public class Paint {
 	}
 		
 
-	private static void printBoard(){
+	private  void printBoard(){
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board[i].length; j++) {
 				System.out.print(board[i][j]);
@@ -53,7 +99,7 @@ public class Paint {
 		}
 	}
 	
-	private static void addNextValidPointInQueue(int i,int j) {
+	private  void addNextValidPointInQueue(int i,int j) {
 		int temp = i + 1;
 		if(temp < boardLength && board[temp][j] != '*') {
 			board[temp][j] = '*';
@@ -75,4 +121,5 @@ public class Paint {
 			queue.add(new Point (i,temp));
 		}
 	}
+	
 }
